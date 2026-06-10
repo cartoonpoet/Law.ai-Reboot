e87e1f74f123afc79763b1ee9d1d9e8d019c05cc
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2026-06-10 09:29:07 +0900

    chore: 로컬 design-system 제거 (@lawkit/ui로 대체 예정)

--- packages/design-system/src/styles/global.css.ts
+++
@@ -1,7 +0,0 @@
-import { globalStyle } from "@vanilla-extract/css";
-import { textColor } from "./tokens";
-
-globalStyle("body", {
-  fontFamily: "Pretendard, sans-serif",
-  color: textColor.body,
-});
